69d3df13968709503d4aa009484177119326e0f2
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-21 23:04:21 +0200

    Created usecases

---
+++ usecases/src/main/java/com/alvaronunez/usecases/GetDrinksByIngredientUC.kt
@@ -0,0 +1,19 @@
+package com.alvaronunez.usecases
+
+import com.alvaronunez.data.Result
+import com.alvaronunez.data.models.DrinkDTO
+import com.alvaronunez.data.repository.Repository
+import com.alvaronunez.domain.models.Drink
+
+class GetDrinksByIngredientUC(private val repository: Repository) {
+    suspend fun invoke(ingredientName: String, onResult: (Result<List<Drink>>) -> Unit) {
+        when(val result: Result<List<DrinkDTO>> = repository.getDrinksByIngredient(ingredientName)) {
+            is Result.Response -> {
+                onResult(Result.Response(result.data.toDrinksList()))
+            }
+            is Result.Error -> {
+                onResult(Result.Error(result.error))
+            }
+        }
+    }
+}
