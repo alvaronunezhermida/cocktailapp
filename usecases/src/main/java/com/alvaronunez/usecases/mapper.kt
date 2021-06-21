233346551c32324eb2b56bdba3f777259d75ce93
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-21 23:45:26 +0200

    Fix drink mapper

--- usecases/src/main/java/com/alvaronunez/usecases/mapper.kt
+++ usecases/src/main/java/com/alvaronunez/usecases/mapper.kt
@@ -6,4 +6,4 @@ import com.alvaronunez.domain.models.Drink
 import com.alvaronunez.domain.models.Ingredient
 
 fun List<IngredientDTO>.toIngredientsList() = map { Ingredient(it.name) }
-fun List<DrinkDTO>.toDrinksList() = map { Drink(it.name) }
+fun List<DrinkDTO>.toDrinksList() = map { Drink(it.name, it.thumb, it.id) }
