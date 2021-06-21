20caee23815f44be8a7eb32b74bc22583e569f31
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-22 00:09:34 +0200

    Fixed remote async function name

--- data/src/main/java/com/alvaronunez/data/service/ServiceDataSource.kt
+++ data/src/main/java/com/alvaronunez/data/service/ServiceDataSource.kt
@@ -19,7 +19,7 @@ class ServiceDataSource(private val service: Service): RemoteDataSource {
     override suspend fun getDrinksByIngredient(ingredientName: String): Result<List<DrinkDTO>> =
         try {
             Result.Response(service.apiService
-                .getDrinksByIngredient(ingredientName).await())
+                .getDrinksByIngredientAsync(ingredientName).await())
         }catch (e: Exception) {
             Result.Error(e.message)
         }
