d9344098127e969111fb187d32aadee9370c93dd
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-22 00:07:37 +0200

    Add remote service implementation

---
+++ data/src/main/java/com/alvaronunez/data/service/ServiceDataSource.kt
@@ -0,0 +1,28 @@
+package com.alvaronunez.data.service
+
+import com.alvaronunez.data.Result
+import com.alvaronunez.data.models.DrinkDTO
+import com.alvaronunez.data.models.IngredientDTO
+import com.alvaronunez.data.source.RemoteDataSource
+
+
+class ServiceDataSource(private val service: Service): RemoteDataSource {
+
+    override suspend fun getIngredients(): Result<List<IngredientDTO>> =
+        try {
+            Result.Response(service.apiService
+                .getIngredientsAsync().await())
+        }catch (e: Exception) {
+            Result.Error(e.message)
+        }
+
+    override suspend fun getDrinksByIngredient(ingredientName: String): Result<List<DrinkDTO>> =
+        try {
+            Result.Response(service.apiService
+                .getDrinksByIngredient(ingredientName).await())
+        }catch (e: Exception) {
+            Result.Error(e.message)
+        }
+
+
+}
