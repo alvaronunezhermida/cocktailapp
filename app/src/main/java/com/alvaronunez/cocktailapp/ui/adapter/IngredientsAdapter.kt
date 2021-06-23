b03f501a23d5fb074d2e47ab0bdb9ac8b5af7060
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-23 09:55:50 +0200

    Added ingredients adapter

---
+++ app/src/main/java/com/alvaronunez/cocktailapp/ui/adapter/IngredientsAdapter.kt
@@ -0,0 +1,37 @@
+package com.alvaronunez.cocktailapp.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.alvaronunez.cocktailapp.R
+import com.alvaronunez.cocktailapp.ui.common.basicDiffUtil
+import com.alvaronunez.cocktailapp.ui.common.inflate
+import com.alvaronunez.domain.models.Ingredient
+
+class IngredientsAdapter :
+    RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+
+    var ingredients: List<Ingredient> by basicDiffUtil(
+        emptyList(),
+        areItemsTheSame = { old, new -> old.name == new.name }
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.item_ingredient, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = ingredients.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ingredient = ingredients[position]
+        holder.bind(ingredient)
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(ingredient: Ingredient) {
+            itemView.findViewById<TextView>(R.id.ingredientName).text = ingredient.name
+        }
+    }
+}
