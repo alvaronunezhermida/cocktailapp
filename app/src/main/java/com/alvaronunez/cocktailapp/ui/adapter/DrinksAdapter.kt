98b92aa8a9da1191d4b98a690cca36b14d2eacbf
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-28 14:49:15 +0200

    Added drinks image

--- app/src/main/java/com/alvaronunez/cocktailapp/ui/adapter/DrinksAdapter.kt
+++ app/src/main/java/com/alvaronunez/cocktailapp/ui/adapter/DrinksAdapter.kt
@@ -1,13 +1,16 @@
 package com.alvaronunez.cocktailapp.ui.adapter
 
+import android.net.Uri
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.alvaronunez.cocktailapp.R
 import com.alvaronunez.cocktailapp.ui.common.basicDiffUtil
 import com.alvaronunez.cocktailapp.ui.common.inflate
 import com.alvaronunez.domain.models.Drink
+import com.bumptech.glide.Glide
 
 class DrinksAdapter :
     RecyclerView.Adapter<DrinksAdapter.ViewHolder>() {
@@ -33,6 +36,9 @@ class DrinksAdapter :
         fun bind(drink: Drink) {
             //TODO: use viewBinding
             itemView.findViewById<TextView>(R.id.drinkName).text = drink.name
+            Glide.with(itemView.context)
+                .load(Uri.parse(drink.thumb))
+                .into(itemView.findViewById(R.id.drinkThumb))
         }
     }
 }
