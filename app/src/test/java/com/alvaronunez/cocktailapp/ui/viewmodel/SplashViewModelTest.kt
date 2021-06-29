97214a8f488804c820df6bf25a6980c490e3fcf6
Álvaro Núñez Hermida <alvaro.nunez@promocionesfarma.com>
2021-06-29 22:40:22 +0200

    SplashViewModel unit tests

---
+++ app/src/test/java/com/alvaronunez/cocktailapp/ui/viewmodel/SplashViewModelTest.kt
@@ -0,0 +1,63 @@
+package com.alvaronunez.cocktailapp.ui.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.alvaronunez.domain.models.Ingredient
+import com.alvaronunez.usecases.GetIngredientsUC
+import com.alvaronunez.data.Result
+import io.mockk.*
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.Dispatchers
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class SplashViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @RelaxedMockK
+    lateinit var getIngredientsUC: GetIngredientsUC
+
+    @RelaxedMockK
+    lateinit var observer: Observer<SplashViewModel.SplashModel>
+
+    lateinit var splashViewModel: SplashViewModel
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        splashViewModel = SplashViewModel(getIngredientsUC, Dispatchers.Unconfined)
+    }
+
+    @Test
+    fun `test view_navigateToIngredients() are called when data is retrieved`() {
+        splashViewModel.model.observeForever(observer)
+
+        val captureCallback = slot<(Result<List<Ingredient>>)-> Unit>()
+        coEvery { getIngredientsUC.invoke(capture(captureCallback)) } answers {
+            val fakeResult = Result.Response(listOf<Ingredient>())
+            captureCallback.captured.invoke(fakeResult)
+        }
+
+        splashViewModel.loadIngredients()
+        coVerify { observer.onChanged(SplashViewModel.SplashModel.NavigateToIngredientsList) }
+    }
+
+    @Test
+    fun `test view_showError() are called when data is retrieved with error`() {
+        splashViewModel.model.observeForever(observer)
+
+        val captureCallback = slot<(Result<List<Ingredient>>)-> Unit>()
+        coEvery { getIngredientsUC.invoke(capture(captureCallback)) } answers {
+            val fakeError = Result.Error()
+            captureCallback.captured.invoke(fakeError)
+        }
+        splashViewModel.loadIngredients()
+        coVerify {
+            observer.onChanged(SplashViewModel.SplashModel.Error(null))
+        }
+    }
+
+}
